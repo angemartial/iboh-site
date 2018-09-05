c560d616d1055b43f390d4935e755b69c7790a3d
angemartial <angemartialkoffi@gmail.com>
2018-09-05 13:55:57 +0000

    investissement

--- src/AppBundle/Form/PropertySearchType.php
+++ src/AppBundle/Form/PropertySearchType.php
@@ -19,61 +19,61 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class PropertySearchType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('location', EntityType::class, [
-                'label' => 'Commune',
-                'class' => Location::class,
-                'choice_label' => 'title',
-                'attr' => [
-                    'class' => 'selectpicker',
-                    'title' => 'Commune',
-                    'data-hide-disabled' => 'true',
-                    'data-live-search' => 'true'
-                ]
-            ])
-            ->add('subLocation', null, [
-                "attr" => [
-                    'placeholder' => 'Quartier'
-                ]
-            ])
-            ->add('minArea', null, [
-                "attr" => [
-                    'placeholder' => 'Superficie min'
-                ]
-            ])
-            ->add('maxArea', null, [
-                "attr" => [
-                    'placeholder' => 'Superficie max'
-                ]
-            ])
-            ->add('bedRooms', null, [
-                "attr" => [
-                    'placeholder' => 'Chambres'
-                ]
-            ])
-            ->add('bathRooms', null, [
-                "attr" => [
-                    'placeholder' => 'Salles de bain'
-                ]
-            ])
-            ->add('priceMin', null, [
-                'label' => 'Prix'
-            ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Trouver'
-            ]);
-    }
+{
+    $builder
+        ->add('location', EntityType::class, [
+            'label' => 'Commune',
+            'class' => Location::class,
+            'choice_label' => 'title',
+            'attr' => [
+                'class' => 'selectpicker',
+                'title' => 'Commune',
+                'data-hide-disabled' => 'true',
+                'data-live-search' => 'true'
+            ]
+        ])
+        ->add('subLocation', null, [
+            "attr" => [
+                'placeholder' => 'Quartier'
+            ]
+        ])
+        ->add('minArea', null, [
+            "attr" => [
+                'placeholder' => 'Superficie min'
+            ]
+        ])
+        ->add('maxArea', null, [
+            "attr" => [
+                'placeholder' => 'Superficie max'
+            ]
+        ])
+        ->add('bedRooms', null, [
+            "attr" => [
+                'placeholder' => 'Chambres'
+            ]
+        ])
+        ->add('bathRooms', null, [
+            "attr" => [
+                'placeholder' => 'Salles de bain'
+            ]
+        ])
+        ->add('priceMin', null, [
+            'label' => 'Prix'
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => 'Trouver'
+        ]);
+}
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Extra\PropertySearch'
-        ));
-    }
+public function configureOptions(OptionsResolver $resolver)
+{
+    $resolver->setDefaults(array(
+        'data_class' => 'AppBundle\Extra\PropertySearch'
+    ));
+}
 
-    public function getBlockPrefix()
-    {
-        return 'appbundle_propertysearch';
-    }
+public function getBlockPrefix()
+{
+    return 'appbundle_propertysearch';
+}
 }
